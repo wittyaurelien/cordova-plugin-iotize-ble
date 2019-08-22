a133efa950ceab54c68f2c8c6c84398c2cb66142
Stéphane SL. LEONARD <stephane.leonard@iotize.com>
2019-08-22 16:45:11 +0200

    fix typings

--- src/www/cordova-interface.ts
+++ src/www/cordova-interface.ts
@@ -8,20 +8,20 @@
 // import { exec } from "cordova";
 export interface CordovaInterface {
 
-    checkAvailable (success: (data: any) => any, failure: (err: any) => any);
+    checkAvailable (success: (data: any) => any, failure: (err: any) => any): void;
 
-    startScan(success: (data: any) => any, failure: (err: any) => any);
+    startScan(success: (data: any) => any, failure: (err: any) => any): void;
 
-    stopScan(success: (data: any) => any, failure: (err: any) => any);
+    stopScan(success: (data: any) => any, failure: (err: any) => any): void;
             
-    connect(device_id: string, success: (data: any) => any, failure: (err: any) => any);
+    connect(device_id: string, success: (data: any) => any, failure: (err: any) => any): void;
         
-    disConnect(device_id: string, success: (data: any) => any, failure: (err: any) => any);
+    disConnect(device_id: string, success: (data: any) => any, failure: (err: any) => any): void;
     
-    isConnected(device_id: string, success: (data: any) => any, failure: (err: any) => any);
+    isConnected(device_id: string, success: (data: any) => any, failure: (err: any) => any): void;
     
-    send(device_id: string, data: any, success: (data: any) => any, failure: (err: any) => any);
+    send(device_id: string, data: any, success: (data: any) => any, failure: (err: any) => any): void;
     
-    getLastError(success: (data: any) => any, failure: (err: any) => any);
+    getLastError(success: (data: any) => any, failure: (err: any) => any): void;
     
 }
